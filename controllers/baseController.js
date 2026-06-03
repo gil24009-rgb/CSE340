e20edaf21fff7836db869f2d6e7700c55274eea3
gil24009-rgb <gil24009@byui.edu>
2026-06-03 16:03:48 -0600

    Apply award-inspired home design

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,6 +1,15 @@
+import { getAllCourses } from "../models/catalogModel.js";
+import { getSortedFaculty } from "../src/models/faculty/faculty.js";
+
 function buildViewModel(title) {
+  const courses = getAllCourses();
+  const sectionCount = courses.reduce((total, course) => total + course.sections.length, 0);
+
   return {
+    courseCount: courses.length,
     enableLiveReload: process.env.NODE_ENV !== "production",
+    facultyCount: getSortedFaculty().length,
+    sectionCount,
     title,
   };
 }
